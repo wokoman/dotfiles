09f5e04a489f56b2666a073e6f8e65ee919b6073
Michal Kozák <wokoman@gmail.com>
2025-08-09 23:54:38 +0200

    Let's go with this Starship config I suppose

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,7 +10,14 @@ end
 -- C O N F I G   T I M E (◔◡◔✿) --
 config.color_scheme = 'Monokai (base16)'
 config.font = wezterm.font('0xProto Nerd Font Mono')
-config.font_size = 18.0
+
+-- Set font size based on operating system
+if wezterm.target_triple:find("darwin") then
+    config.font_size = 18.0 -- macOS
+else
+    config.font_size = 14.0 -- Linux and other platforms
+end
+
 config.keys = {
     -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
     { key = 'LeftArrow',  mods = 'OPT', action = act.SendString '\x1bb' },
